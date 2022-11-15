14f63a0777918333917464235bea8eb2a857d9e3
anfalcrypto <100465461+anfalcrypto@users.noreply.github.com>
2022-11-15 15:33:59 +0530

    Added

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: '0.8.17',
+  networks: {
+    mumbai: {
+      url: process.env.QUICKNODE_HTTP_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
+
+
